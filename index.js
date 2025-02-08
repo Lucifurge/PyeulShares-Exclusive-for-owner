f1e9acf81d8f6b42cb76d2ac422dac41cbc21e5a
Lucifurge <tasogaresaya@gmail.com>
2025-02-08 13:38:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     lockScreen();
 
-    document.getElementById("shareForm").addEventListener("submit", function (e) {
+     document.getElementById("shareForm").addEventListener("submit", function (e) {
         e.preventDefault();
 
         // Get form data
